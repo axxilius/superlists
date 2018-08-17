c7d31d0435eae0373f3c54a3f3051290365f322e
axxil <axxil@yandex.ru>
2018-08-17 12:36:25 +0300

    added home_page

--- superlists/urls.py
+++ superlists/urls.py
@@ -1,3 +1,4 @@
+from lists import views
 """superlists URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
@@ -17,5 +18,5 @@ from django.conf.urls import url
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^admin/', admin.site.urls),
+    url(r'^$', views.home_page, name='home'),
 ]
